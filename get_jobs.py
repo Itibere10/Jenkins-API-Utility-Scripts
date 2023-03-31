0f46447fe67761a4c0c7ba6eda41b35999b62eaf
Itiberê Gonçalves Silva Filho <56208580+Itibere10@users.noreply.github.com>
2023-03-31 13:02:29 -0300

    Create get_jobs.py

---
+++ get_jobs.py
@@ -0,0 +1,46 @@
+# Declarando as bibliotecas
+import json
+import requests
+import pandas as pd
+import numpy as np
+import pytz
+import getpass
+from datetime import datetime
+
+# Dados de entrada:
+usuario = 'USERNAME'
+senha = 'PASSWORD'
+url = 'http://jenkins.com.br/job/Repo/job/Pasta/'
+
+#Autenticando a comunicação e acesso ao servidor
+auth = (usuario,senha)
+
+#Realizando a requisição dos jobs pela API do Jenkins
+url = url + 'api/json?tree=jobs[name,jobs[name,url]]&pretty=true'
+response = requests.get(url, auth=auth)
+python_obj = response.json()
+
+#Declarando as listas utilizadas (se necessário)
+#repos = []
+#branches_nomes = []
+#branches_urls = []
+mapa = []
+
+# Realizando o loop de mapeamento
+for x in range(len(python_obj['jobs'])):
+    #repos.append(python_obj['jobs'][x]['name'])
+    for y in range(len(python_obj['jobs'][x]['jobs'])):
+        #branches_nomes.append(python_obj['jobs'][x]['jobs'][y]['name'])
+        #branches_urls.append(python_obj['jobs'][x]['jobs'][y]['url'])
+        mapa.append([[python_obj['jobs'][x]['name']],python_obj['jobs'][x]['jobs'][y]['name'],python_obj['jobs'][x]['jobs'][y]['url']])
+
+# Configurando e ajustando o DataFrame
+df = pd.DataFrame(mapa)
+df.columns = ['Repositorio','Branch','URL']
+df['Repositorio'] = df['Repositorio'].astype(str).str.replace("[","").str.replace("]","")
+df['Repositorio'] = df['Repositorio'].str.replace("'", "")
+
+data = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d-%m-%Y_%H-%M')
+df.to_csv('jobs-'+data+'.xlsx', index=False)
+
+print(df)
